d90cf72a4eef3f0ba1d02bcd1f6b7285c3008caf
Welington Cruz de Assis <welingtoncassis@gmail.com>
2020-07-04 02:41:19 -0300

    feat: add code to get message and put in dynamodb

--- handler.js
+++ handler.js
@@ -1,11 +1,37 @@
 'use strict';
 const settings = require('./config/settings')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+const uuid = require('uuid')
+
 
 class Handler {
   static async main(event) {
     console.log('at', new Date().toISOString(), JSON.stringify(event, null, 2))
+    const { data } = await axios.get(settings.commitMessageUrl)
+    const $ = cheerio.load(data)
+    const [commitMessage] = await $("#content").text().trim().split('\n')
+    console.log('Message', commitMessage)
+
+    const params = {
+      TableName: settings.dbTableName,
+      Item: {
+        commitMessage,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString()
+      }
+    }
+
+    await dynamoDB.put(params).promise()
+    console.log('Process finished at', new Date().toISOString())
     return {
       statusCode: 200
     }
   }
+}
+
+module.exports = {
+  scheduler : Handler.main
 }
